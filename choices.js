f303754f043308089552b3d5e1910decde17caea
Irene <ireneho.imyh@gmail.com>
2018-07-25 14:37:05 -0700

    added another question

--- choices.js
+++ choices.js
@@ -19,21 +19,48 @@ choice1 OR choice 2
 get and display appropriate choice msg
 have go back button
 */
-var choice1 = ["have eyes for ears?"];
-var msg1 = ["You'll see very well!"];
+var choice1 = ["have eyes for ears", "have one million dollars"];
+var msg1 = ["You'll see very well!", "You'd be very rich!"];
 
-var choice2 = ["have ears for eyes?"];
-var mgs2 = ["You'll hear very well!"];
+var choice2 = ["have ears for eyes", "never get sick"];
+var msg2 = ["You'll hear very well!", "Your health would be perfect!"];
 
-var randNum = Math.floor(Math.random() * (choice1.length));
+// putting this here won't work because each html page load
+// will reset the value
+// var randNum = Math.floor(Math.random() * (choice1.length));
+
+function setRandNum() {
+  // sets the random number we'll be using
+  // necessary to put into a function because
+  // otherwise each html page will reload the .js file
+  // and reset our randNum
+  var randNum = Math.floor(Math.random() * (choice1.length));
+  // save randNum into local storage
+  localStorage.setItem("randNum", randNum);
+}
 
 function getChoice1() {
   // find button1 and give it a value from choice1
-  //document.getElementById('button1').value = choice1[randNum];
+  var randNum = localStorage.getItem("randNum");
   document.getElementById('button1').value = choice1[randNum];
 }
 
 function getChoice2() {
   // find button2 and give it a value from choice2
+  var randNum = localStorage.getItem("randNum");
   document.getElementById('button2').value = choice2[randNum];
+}
+
+function getMsg1() {
+  // find header and give it a msg from msg1
+  // use innerHTML to write to choice1.html
+  var randNum = localStorage.getItem("randNum");
+  document.getElementById('msg1').innerHTML = msg1[randNum];
+}
+
+function getMsg2() {
+  // find header and give it a msg from msg2
+  // use innerHTML to write to choice2.html
+  var randNum = localStorage.getItem("randNum");
+  document.getElementById('msg2').innerHTML = msg2[randNum];
 }
