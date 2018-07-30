3ec2e73ea75e34a713e2335c6d42a0ad82e4aaa3
Irene <ireneho.imyh@gmail.com>
2018-07-30 14:11:23 -0700

    added more templates

---
+++ templates/choices-template.js
@@ -0,0 +1,39 @@
+// array for first choices
+var choice1 = [
+];
+// array for messages corresponding to first choices
+var msg1 = [
+];
+
+// array for second choices
+var choice2 = [
+];
+// array for messages corresponding to second choices
+var msg2 = [
+];
+
+function setRandNum() {
+  // set a random number for us to use
+  var randNum = /* FIXME */
+
+  // save randNum into local storage
+  localStorage.setItem("randNum", randNum);
+}
+
+function getChoice1() {
+  // find button1 and give it a value from choice1
+}
+
+function getChoice2() {
+  // find button2 and give it a value from choice2
+}
+
+function getMsg1() {
+  // find header and give it a msg from msg1
+  // use innerHTML to write to choice1.html
+}
+
+function getMsg2() {
+  // find header and give it a msg from msg2
+  // use innerHTML to write to choice2.html
+}
